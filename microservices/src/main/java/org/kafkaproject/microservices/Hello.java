3874ec3ec383cb59ff554e1cb318abf6ca4a07aa
xiaoyulyu2020 <lyu.xiaoyu2@mail.dcu.ie>
2024-08-14 13:43:43 +0100

    get, post

--- microservices/src/main/java/org/kafkaproject/microservices/Hello.java
+++ microservices/src/main/java/org/kafkaproject/microservices/Hello.java
@@ -1,6 +1,8 @@
 package org.kafkaproject.microservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,10 @@ public class Hello {
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello World";
+    }
+
+    @PostMapping("/hello")
+    public String sayHelloPost(@RequestBody String name) {
+        return "Hello " + name + "!";
     }
 }
