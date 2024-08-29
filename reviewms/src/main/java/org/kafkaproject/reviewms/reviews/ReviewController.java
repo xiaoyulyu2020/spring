57a3a820869861593f7339927e01306498d0e14d
xiaoyulyu2020 <lyu.xiaoyu2@mail.dcu.ie>
2024-08-29 19:21:23 +0100

    adjust the url of review micro

--- reviewms/src/main/java/org/kafkaproject/reviewms/reviews/ReviewController.java
+++ reviewms/src/main/java/org/kafkaproject/reviewms/reviews/ReviewController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/companies/{companyId}")
+@RequestMapping("/reviews")
 public class ReviewController {
 
     private final ReviewService reviewService;
@@ -14,28 +14,28 @@ public class ReviewController {
         this.reviewService = reviewService;
     }
 
-    @GetMapping("/reviews")
-    public List<Review> getReviews(@PathVariable Long companyId) {
+    @GetMapping
+    public List<Review> getReviews(@RequestParam Long companyId) {
         return reviewService.getReviews(companyId);
     }
-
-    @PostMapping("/reviews")
-    public boolean addReviews(@PathVariable Long companyId, @RequestBody Review review) {
+// /reviews?companyId={companyId}
+    @PostMapping
+    public boolean addReviews(@RequestParam Long companyId, @RequestBody Review review) {
         return reviewService.addReview(companyId, review);
     }
 
-    @GetMapping("/reviews/{reviewId}")
-    public Review getReview(@PathVariable Long companyId, @PathVariable Long reviewId) {
+    @GetMapping("/{reviewId}")
+    public Review getReview(@PathVariable Long reviewId) {
         return reviewService.getReview(reviewId);
     }
 
-    @PutMapping("/reviews/{reviewId}")
+    @PutMapping("/{reviewId}")
     public boolean updateReview(@PathVariable Long reviewId, @RequestBody Review review) {
         return reviewService.updateReview(reviewId, review);
     }
 
-    @DeleteMapping("/reviews/{reviewId}")
-    public boolean deleteReview(@PathVariable Long companyId, @PathVariable Long reviewId) {
+    @DeleteMapping("/{reviewId}")
+    public boolean deleteReview(@PathVariable Long reviewId) {
         return reviewService.deleteReview(reviewId);
     }
 }
