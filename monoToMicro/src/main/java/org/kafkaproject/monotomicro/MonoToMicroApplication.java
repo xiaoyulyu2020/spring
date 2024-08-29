14c6181d9869f9dae0df2e3810c52752f40e7a32
xiaoyulyu2020 <lyu.xiaoyu2@mail.dcu.ie>
2024-08-29 15:14:54 +0100

    mono to Micro

---
+++ monoToMicro/src/main/java/org/kafkaproject/monotomicro/MonoToMicroApplication.java
@@ -0,0 +1,13 @@
+package org.kafkaproject.monotomicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonoToMicroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonoToMicroApplication.class, args);
+    }
+
+}
---
+++ monoToMicro/src/test/java/org/kafkaproject/monotomicro/MonoToMicroApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kafkaproject.monotomicro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonoToMicroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
